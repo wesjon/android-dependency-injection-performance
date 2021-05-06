79f1789b61b7aab6db47748c164bf700897f02d3
Wesley Jonathan Marcolino <w.jonathan.marcolino@accenture.com>
2021-05-06 13:47:03 -0300

    Update libraries

--- app/src/main/java/com/sloydev/dependencyinjectionperformance/InjectionTest.kt
+++ app/src/main/java/com/sloydev/dependencyinjectionperformance/InjectionTest.kt
@@ -15,16 +15,18 @@ import com.sloydev.dependencyinjectionperformance.koin.koinKotlinModule
 import org.kodein.di.Kodein
 import org.kodein.di.direct
 import org.kodein.di.erased.instance
-import org.koin.core.KoinComponent
+import org.koin.core.component.KoinApiExtension
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.get
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
-import org.koin.core.get
 import org.rewedigital.katana.Component
 import org.rewedigital.katana.Katana
 import org.rewedigital.katana.android.environment.AndroidEnvironmentContext
 import org.rewedigital.katana.android.environment.AndroidEnvironmentContext.Profile.SPEED
 import javax.inject.Inject
 
+@KoinApiExtension
 class InjectionTest : KoinComponent {
 
     private val kotlinDaggerTest = KotlinDaggerTest()
